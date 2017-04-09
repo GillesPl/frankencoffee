f33c0bc3a2ccc79c256b22f1bfa799fcbe541ba5
Gilles <gilles.platteeuw@student.howest.be>
2017-04-09 14:34:53 +0200

    Added ability to execute python, added some idea’s for checking water via node & python

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var app = express();
 var path = require('path');
+var PythonShell = require('python-shell');//initializing pythonshell
 
 
+//mongo connection
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
@@ -9,7 +12,7 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/frankencoffee';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
@@ -19,33 +22,53 @@ MongoClient.connect(url, function(err, db) {
 
 //create router object
 var router = express.Router();
-
-
 module.exports = router;
 
 //home
-router.get('/', function(req, res) {  
+router.get('/', function (req, res) {
   res.render('pages/index', {
-    title : 'Home'
+    title: 'Home'
   });
 });
 
-//about
-router.get('/about',function(req,res){
- res.render('pages/about' , {
-   title : 'About'
- });
+//controls
+router.get('/controls', function (req, res) {
+  res.render('pages/controls', {
+    title: 'Controls'
+  });
 });
 
 //contact
-router.get('/contact',function(req,res){
-    res.render('pages/contact', {
-      title : 'Contact'
-    });
+router.get('/contact', function (req, res) {
+  res.render('pages/contact',  {
+    title: 'Contact'
+  });
+
+  var pyshell = new PythonShell("/public/test.py");
+
+  pyshell.send('Downey is a fggit');
+
+  pyshell.on('message', function (message) {
+    //ontvangt msg van python (print)
+    console.log(message);
+  });
+
+  //stopt het process
+  pyshell.end(function (err) {
+    if (err) throw err;
+    console.log('finished');
+  });
+
+
+  /*PythonShell.run("/public/test.py", function (err) {
+     if (err) throw err;
+     console.log('finished');
+   });*/
 });
 
-//contact post
-router.post('/contact',function(req,res){
-    
-})
+router.get("/refreshwater",function(req,res) {
+  //execute python script check in the controls get
+  res.redirect("/controls");
+});
+
 
--- server.js
+++ server.js
@@ -4,19 +4,29 @@ var ejs = require('express-ejs-layouts');
 var app = express();
 var port = 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //use ejs en express layouts
 app.set('view engine','ejs');
 app.use(ejs);
 
+//set static files (css, images)
+app.use(express.static(__dirname + '/public'));
+
+app.use('/nm', express.static('/node_modules'))
+
+
 var router = require('./app/routes');
 //express middleware
 app.use('/' , router);
 
 
-//set static files (css, images)
-app.use(express.static(__dirname + '/public'));
+
 
 // start the server
 app.listen(port, function() {
